34fff519850cd8deeda00a9227c7e85cc7a3479b
mingina <mathea.mingina@busaracenter.org>
2022-07-20 15:21:53 +0300

    Max value prints

---
+++ IntMaxValue.java
@@ -0,0 +1,15 @@
+public class IntMaxValue {
+    private static final int MAX_VALUE = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        int a = MAX_VALUE;
+        System.out.println(a);
+        System.out.println(a + 1);
+        System.out.println(2 - a);
+        System.out.println(-2 - a);
+        System.out.println(2 * a);
+        System.out.println(4 * a);
+
+    }
+
+}
