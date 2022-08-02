42032d04169638a79aca6873f94c5d73c647edb5
mingina <mathea.mingina@busaracenter.org>
2022-08-02 11:11:20 +0300

    old exercise

---
+++ Flip.java
@@ -0,0 +1,9 @@
+public class Flip {
+    public static void main(String[] args) throws Exception {
+        if (Math.random() < 0.5) {
+            System.out.println("Tails");
+        } else {
+            System.out.println("Heads");
+        }
+    }
+}
---
+++ TenHellos.java
@@ -0,0 +1,13 @@
+public class TenHellos {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1st Hello");
+        System.out.println("2nd Hello");
+        System.out.println("3rd Hello");
+        int i = 4;
+        while (i <= 10) {
+            System.out.println(i + "th Hello");
+            i++;
+        }
+
+    }
+}
