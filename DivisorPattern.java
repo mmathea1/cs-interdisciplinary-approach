c566d1ed3e95f7ad77ff1888f642cdef9c824ee4
mingina <mathea.mingina@busaracenter.org>
2022-08-02 13:15:46 +0300

    print binary instead

--- DivisorPattern.java
+++ DivisorPattern.java
@@ -4,12 +4,12 @@ public class DivisorPattern {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if ((i % j == 0) || (j % i == 0)) {
-                    System.out.print("* ");
+                    System.out.print("1");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("0");
                 }
             }
-            System.out.println(i);
+            System.out.println(" " + i);
         }
     }
 }
