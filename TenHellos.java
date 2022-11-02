f945e5f252efe133a00de2b57a891dd7d9697f94
mingina <mathea.mingina@busaracenter.org>
2022-11-02 10:37:23 +0300

    improved TenHellos

--- TenHellos.java
+++ TenHellos.java
@@ -3,10 +3,24 @@ public class TenHellos {
 
         int j = Integer.parseInt(args[0]);
         int i = 1;
-        while (i <= j) {
-            if() i 
+        do {
+            switch (i) {
+                case 1:
+                    System.out.println(i + "st Hello");
+                    break;
+                case 2:
+                    System.out.println(i + "nd Hello");
+                    break;
+                case 3:
+                    System.out.println(i + "rd Hello");
+                    break;
+                default:
+                    System.out.println(i + "th Hello");
+                    break;
+            }
             i++;
-        }
+
+        } while (i <= j);
 
     }
 }
