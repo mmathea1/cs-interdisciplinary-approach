35970325df24e77e5a2dc05e767b21e9156bbd27
mingina <mathea.mingina@busaracenter.org>
2023-01-05 15:02:44 +0300

    updated ruler2 file, deleting chapter-one branch

--- Ruler2.java
+++ Ruler2.java
@@ -1,11 +1,10 @@
 public class Ruler2 {
     public static void main(String[] args) {
-        String ruler = "1";
+        String ruler = " ";
         int n = Integer.parseInt(args[0]);
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
+            ruler = ruler + i + ruler;
             System.out.println(ruler);
-            ruler = ruler + " " + i + " " + ruler;
         }
-        System.out.println(ruler);
     }
 }
