a7011f0733c94ad77b46f5c826bf4589756da404
mingina <mathea.mingina@busaracenter.org>
2023-03-16 19:38:12 +0300

    added new exercises for section1.3

---
+++ Ex1313.java
@@ -0,0 +1,11 @@
+public class Ex1313 {
+    public static void main(String[] args) {
+        int n = 123456789;
+        int m = 0;
+        while (n != 0) {
+            m = (10 * m) + (n % 10);
+            n = n / 10;
+        }
+        System.out.println("m = " + m + " n = " + n);
+    }
+}
