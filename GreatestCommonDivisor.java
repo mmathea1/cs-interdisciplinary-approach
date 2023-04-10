bd924d8b86f036f21ae1573e8c5e888f293f6682
mingina <mathea.mingina@busaracenter.org>
2023-04-10 22:19:32 +0300

    cleanup

---
+++ GreatestCommonDivisor.java
@@ -0,0 +1,7 @@
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        
+    }
+}
