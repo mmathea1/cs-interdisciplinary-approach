b0c6e5f10dbc6cb83d0d72524738e70662cb49fe
Mingina Mathea <mingina.mathea075@gmail.com>
2023-05-16 15:06:46 +0300

    updated gcd code

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -2,6 +2,17 @@ public class GreatestCommonDivisor {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
-        
+        int rem = 1;
+        int gcd = 1;
+        while (rem > 0) {
+            rem = x % y;
+            x = y;
+
+            if (rem == 0) {
+                gcd = y;
+            }
+            y = rem;
+        }
+        System.out.println("gcd = " + gcd);
     }
 }
