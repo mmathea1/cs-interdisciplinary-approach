2c06fd877bd8723712bc2638e9c6c51343ff5b8c
Mingina Mathea <mingina.mathea075@gmail.com>
2023-06-05 20:24:48 +0300

    added MarginalTaxRate

--- Sqrt.java
+++ Sqrt.java
@@ -1,7 +1,7 @@
 public class Sqrt {
     public static void main(String[] args) {
         double c = Double.parseDouble(args[0]);
-        double EPSILON = 1e-15;
+        double EPSILON = 1.0e-15;
         double t = c;
         while (Math.abs(t - c / t) > EPSILON * t) {
             t = (c / t + t) / 2.0;
